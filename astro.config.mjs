dc750394f8a4b95b6340a2945d48e85ef80f5914
miriamsobrino <miriamsobrinodev@gmail.com>
2024-06-03 10:11:29 +0200

    change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://miriamsobrino.github.io',
+    base: '/world-flags/',
   
 });
